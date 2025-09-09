d348852eb9535bfa74d0047369235c920dd859dd
IsaAnnaqibAlhasni <250605110207@student.uin-malang.ac.id>
2025-09-10 01:08:34 +0700

    Initial commit

---
+++ src/biodata/md/BiodataMd.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package biodata.md;
+
+/**
+ *
+ * @author ISA
+ */
+public class BiodataMd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Nama                : Isa Annaqib Alhasni");
+       System.out.println("TTL                 : Gorontalo, 29 Agustus 2007");
+       System.out.println("Alamat              : Mahad kampus 3 UIN malang");
+       System.out.println("No Telepon          : 082345625552");
+       System.out.println("Pendidikan Terakhir : SMKs Nurul Haromain");
+       System.out.println("Hobi                : Gym, Gaming, Bernyanyi");      
+      
+    }
+    
+}
